17038da09dac0e8a491debccb1fbbeb17eb37fba
karthikvasudevan <karthikwasudevan@gmail.com>
2019-01-07 04:20:08 +0530

    created example local_config.py

---
+++ config_local_example.py
@@ -0,0 +1,10 @@
+LOCAL_CONFIG = {
+    'cam_id' : 'Sony',
+    'app_directory' : 'tl_home',
+    'shoots_directory' : 'shoots',
+    'picture_count_limit' : 2000,
+    'scan_count' : 0,
+    'debug' : False,
+    'app_directory_exists' : False,
+    'shoots_folder_exists' : False,
+}
