09c7a42c94ba30827ee61d8786596c98defab642
karthikvasudevan <karthikwasudevan@gmail.com>
2019-01-07 02:32:10 +0530

    created a local congif file override in config.py

--- config.py
+++ config.py
@@ -3,14 +3,6 @@ from datetime import datetime, timedelta
 from pprint import pprint
 
 class Config:
-    cam_id = 'Sony'
-    app_directory = 'tl_home'
-    shoots_directory = 'shoots'
-    picture_count_limit = 2000
-    scan_count = 0
-    debug = False
-    app_directory_exists = False
-    shoots_folder_exists = False
 
     def __init__(self):
         self.date = datetime.now().strftime("%H_%M_%S")
@@ -18,10 +10,36 @@ class Config:
         self.desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop/')
         self.app_setup()
 
+    def local_config(self,LOCAL_CONFIG):
+        print(LOCAL_CONFIG)
+        self.cam_id = LOCAL_CONFIG['cam_id']
+        self.app_directory = LOCAL_CONFIG['app_directory']
+        self.shoots_directory = LOCAL_CONFIG['shoots_directory']
+        self.picture_count_limit = LOCAL_CONFIG['picture_count_limit']
+        self.scan_count = LOCAL_CONFIG['scan_count']
+        self.debug = LOCAL_CONFIG['debug']
+        self.app_directory_exists = LOCAL_CONFIG['app_directory_exists']
+        self.shoots_folder_exists = LOCAL_CONFIG['shoots_folder_exists']
+
+    def config_default(self):
+        try:
+            import config_local
+            self.local_config(config_local.LOCAL_CONFIG)
+        except ImportError:
+            self.cam_id = 'Sony'
+            self.app_directory = 'tl_home'
+            self.shoots_directory = 'shoots'
+            self.picture_count_limit = 2000
+            self.scan_count = 0
+            self.debug = False
+            self.app_directory_exists = False
+            self.shoots_folder_exists = False
+
     def __str__(self):
         return self.current_folder_path
 
     def app_setup(self):
+        self.config_default()
         self.app_folder()
         self.shoots_folder()
         self.year_folder()
